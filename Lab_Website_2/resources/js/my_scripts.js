78b4d7ddbb5fd164491a7ba4c10691bad4c1be10
josp9970 <47039405+josp9970@users.noreply.github.com>
2019-02-24 20:57:44 -0700

    Add files via upload

---
+++ Lab_Website_2/resources/js/my_scripts.js
@@ -0,0 +1,227 @@
+/*
+	players is an array to hold each player's information.
+	Fields:
+		name - Football player's name
+		img  - The relative/absolute path to the image file.
+		alt  - The alternative text that describes the image.
+		year - The student's year in college (Freshman, Sophomore, Junior, Senior).
+		major- The student's current college major.
+		games_played    - The number of football games the student has played for the Buffs.
+		pass_yards      - The total number of passing yards in the student's football career for the Buffs.
+		rushing_yards   - The total number of rushing yards in the student's football career for the Buffs.
+		receiving_yards - The total number of receiving yards in the student's football career for the Buffs.
+*/
+var players = [{name:"John Doe", img: "../resources/img/player1.jpg", alt:"Image of Player 1", year:"Sophomore", major:"Art", games_played: 23, pass_yards: 435, rushing_yards: 200, receiving_yards: 88},
+				{name:"James Smith", img: "../resources/img/player2.jpg", alt:"Image of Player 2", year:"Junior", major:"Science", games_played: 17, pass_yards: 192, rushing_yards: 102, receiving_yards: 344},
+				{name:"Samuel Phillips", img: "../resources/img/player3.jpg", alt:"Image of Player 3", year:"Freshman", major:"Math", games_played: 8, pass_yards: 35, rushing_yards: 70, receiving_yards: 98},
+				{name:"Robert Myers", img: "../resources/img/player4.jpg", alt:"Image of Player 4", year:"Senior", major:"Computer Science", games_played: 31, pass_yards: 802, rushing_yards: 375, receiving_yards: 128}];
+
+
+/*
+	Registration Page:
+		viewStudentStats(id, toggle) method
+			parameters:
+				id - The css id of the html tag being updated.
+				toggle -
+					0 - hide the html tag
+					1 - make the html tag visible
+
+			purpose: This method will accept the id of an html tag and a toggle value.
+					 The method will then set the html tag's css visibility and height.
+					 To hide the html tag (toggle - 0), the visibility will be set to hidden and
+					 the height will be set to 0.
+					 To reveal the html tag (toggle - 1), the visibility will be set to visible and
+					 the height will be set to auto.
+*/
+
+function viewStudentStats(id, toggle) {
+	if (toggle == 0){
+		document.getElementById(id).style.visibility = "hidden";
+		document.getElementById(id).style.height = "0px";
+	}else {
+		document.getElementById(id).style.visibility = "visible";
+		document.getElementById(id).style.visibility = "auto";
+	}
+  return;
+}
+
+/*
+	Home Page:
+		changeColor(color) method
+			parameter:
+				color- A css color
+
+			purpose: This method will set the html body's background color to the
+					 provided parameter.
+*/
+
+function changeColor(color){
+	document.body.style.backgroundColor = color;
+}
+
+/*
+	Football Season Stats Page:
+		loadStatsPage method:
+			parameters: none
+
+			purpose: This method will iterate through the stats table and
+					 do the following:
+						1. Read through each row of the table & determine which team won
+						   the game.
+
+						2. Update the winner column to the name of the winning team.
+
+						3. Keep track of the number of wins/losses for the Buffs.
+
+						4. Update the second table to show the total number of wins/losses for the Buffs.
+*/
+
+function loadStatsPage() {
+	// iterate through the table
+	var homeDubs = 0;
+	var awayDubs = 0;
+	var table = document.getElementById("stats_table");
+	// table.rows[0].cells[0].innerHTML = "test";
+	// start at row 2
+	for (var i = 2, row; row = table.rows[i]; i++) {
+		homeScore = table.rows[i].cells[2].innerHTML;
+		homeScore = parseInt(homeScore);
+		awayScore = table.rows[i].cells[3].innerHTML;
+		awayScore = parseInt(awayScore);
+
+		if (homeScore == 7){
+			console.log("Home: "+ homeScore + " Away: " + awayScore);
+		}
+
+		if (homeScore > awayScore){
+			table.rows[i].cells[4].innerHTML = "Colorado";
+			homeDubs++;
+		}else if (homeScore < awayScore){
+
+			opposingTeam = table.rows[i].cells[1].innerHTML;
+			table.rows[i].cells[4].innerHTML = opposingTeam;
+			awayDubs++;
+		}
+
+   }
+
+	 // update the winner column
+
+	 var winCol = document.getElementById("wins");
+	 winCol.innerHTML = homeDubs;
+	 var losCol = document.getElementById("losses");
+	 losCol.innerHTML = awayDubs;
+	 // var cell1 = winCol.insertCell(0);
+
+
+}
+
+/*
+	Football Player Information Page
+		loadPlayersPage method:
+			parameters: none
+
+			purpose: This method will populate the dropdown menu to allow the
+					 user to select which player's information to view.
+
+					 To handle this, you will need to iterate through the players array
+					 and do the following for each player:
+						1. Create an anchor tag
+						2. Set the href to "#", this will make sure the
+							anchor tag doesn't change pages
+						3. Set the onclick to call switchPlayers method
+							(this will need to pass in the index inside the players array)
+						4. Set the anchor tag's text to the player's name.
+
+					After setting all of the anchor tags, update the innerHTML of the dropdown menu.
+					As a note, the id for the dropdown menu is player_selector.
+
+		switchPlayers(playerNum) method:
+			parameters:
+				playerNum - The index of the football player in the players array.
+
+			purpose:
+				This method will update the the spans on the player's information pageX
+				and calculate the average passing, rushing, and receiving yards.
+
+				Span ids:
+					p_year     - the player's year in college
+					p_major    - the player's major in college
+					g_played   - the number of games played for Buffs
+					player_img - the player's photo (must set src and alt)
+					p_yards    - the number of passing yards
+					r_yards    - the number of rushing yards
+					rec_yards  - the number of receiving yards
+
+					Calculated values:
+					  avg_p_yards   - the average number of passing yards for the player's Buff career
+					  avg_r_yards   - the average number of rushing yards for the player's Buff career
+					  avg_rec_yards - the average number of receiving yards for the player's Buff career
+*/
+
+function loadPlayersPage(){
+	var dropDown = document.getElementById("player_selector");
+	// manually fill the dropDown
+	var aTag = document.createElement('a');
+	aTag.setAttribute('href', "#");
+	aTag.onclick = function(){switchPlayers(0)}
+	aTag.innerHTML = players[0]["name"];
+	dropDown.appendChild(aTag);
+
+	var bTag = document.createElement('b');
+	bTag.setAttribute('href', "#");
+	bTag.onclick = function(){switchPlayers(1)}
+	bTag.innerHTML = players[1]["name"];
+	dropDown.appendChild(bTag);
+
+	var cTag = document.createElement('c');
+	cTag.setAttribute('href', "#");
+	cTag.onclick = function(){switchPlayers(2)}
+	cTag.innerHTML = players[2]["name"];
+	dropDown.appendChild(cTag);
+
+	var dTag = document.createElement('d');
+	dTag.setAttribute('href', "#");
+	dTag.onclick = function(){switchPlayers(3)}
+	dTag.innerHTML = players[3]["name"];
+	dropDown.appendChild(dTag);
+
+
+}
+
+function test(){
+	console.log("output");
+}
+
+function switchPlayers(playerNum) {
+
+// var players = [{name:"John Doe", img: "../resources/img/player1.jpg", alt:"Image of Player 1", year:"Sophomore", major:"Art", games_played: 23, pass_yards: 435, rushing_yards: 200, receiving_yards: 88},
+
+	console.log(playerNum);
+	var yr = document.getElementById("p_year");
+	var major = document.getElementById("p_major");
+	var played = document.getElementById("g_played");
+	var pyards = document.getElementById("p_yards");
+	var ryards = document.getElementById("r_yards");
+	var recyds = document.getElementById("rec_yards");
+	var playerimg = document.getElementById("player_img");
+	yr.innerHTML = players[playerNum]["year"];
+	major.innerHTML = players[playerNum]["major"];
+	played.innerHTML = players[playerNum]["games_played"];
+	pyards.innerHTML = players[playerNum]["pass_yards"];
+	ryards.innerHTML = players[playerNum]["rushing_yards"];
+	recyds.innerHTML = players[playerNum]["receiving_yards"];
+	playerimg.src = players[playerNum]["img"];
+	playerimg.alt = players[playerNum]["alt"];
+
+	var avgpyards = document.getElementById("avg_p_yards");
+	var avgryards = document.getElementById("avg_r_yards");
+	var avgrecyards = document.getElementById("avg_rec_yards");
+
+
+	var gamesPlayed = players[playerNum]["games_played"];
+	avgpyards.innerHTML = parseInt((players[playerNum]["pass_yards"])/gamesPlayed);
+	avgryards.innerHTML = parseInt((players[playerNum]["rushing_yards"])/gamesPlayed);
+	avgrecyards.innerHTML = parseInt((players[playerNum]["receiving_yards"])/gamesPlayed);
+
+}
